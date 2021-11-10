520eeda7f41db2e108d73a5752dfd85b9e0d4d83
Will Farhat <35930179+willf668@users.noreply.github.com>
2021-11-09 21:40:20 -0800

    added backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let url:string=window.location.href;
+
+  return (
+    <div className="App">
+      <a className="clickable" id="startButton" href={url+"start"}>
+        Start
+      </a>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import { Router, RouteComponentProps } from "@reach/router";
+
+const RouterPage = (
+  props: { pageComponent: JSX.Element } & RouteComponentProps
+) => props.pageComponent;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <RouterPage path="/" pageComponent={<App/>} />
+      <RouterPage path="/profile" pageComponent={<App/>} />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
